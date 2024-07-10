73be1a36188edcdc01f6f2b38c76489608b273fc
Eric Xue <widefin@gmail.com>
2024-07-10 16:56:39 -0400

    NUMBA=HELL

--- orofacial/orofacial/tongue_tip_track_2D.py
+++ orofacial/orofacial/tongue_tip_track_2D.py
@@ -1,48 +1,84 @@
 import numpy as np 
 from matplotlib import pyplot as plt
+from numba import njit, jit, prange 
 
-def angle(v1, v2):
-    if (np.linalg.norm(v1) == 0 or np.linalg.norm(v2) == 0):
-        return 0
-    return np.degrees(np.math.acos(np.clip(np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2)), -1, 1)))
+# https://stackoverflow.com/questions/57500001/numba-failure-with-np-mean
+@njit
+def centroid(a):
+    return np.array([np.mean(a[:, 0]), np.mean(a[:, 1])])
 
-def is_boundary(img, p):
-    if img[p[1]][p[0]] == 0:
-        return False
-    for dy in range(-1, 2):
-        for dx in range(-1, 2):
-            if p[0]+dx<img.shape[1] and p[1]+dy<img.shape[0] and p[0]+dx>=0 and p[1]+dy>=0 and img[p[1] + dy][p[0] + dx] == 0:
-                return True
-    return False
+@njit
+def clip(a, min_val, max_val):
+    return np.minimum(np.maximum(a, min_val), max_val)
 
+@njit
+def v_is_boundary(img, a):
+    ans = np.zeros(a.shape[0], dtype=np.uint8)
+    for i in prange(a.shape[0]):
+        if img[a[i][1]][a[i][0]] == 0:
+            continue 
+        for dy in range(-1, 2):
+            for dx in range(-1, 2):
+                if a[i][0]+dx<img.shape[1] and a[i][1]+dy<img.shape[0] and a[i][0]+dx>=0 and a[i][1]+dy>=0 and img[a[i][1] + dy][a[i][0] + dx] == 0:
+                    ans[i] = 1 
+    return ans
+
+@njit
+def v_norm(a):
+    ans = np.zeros(a.shape[0], dtype=np.float64)
+    for i in prange(a.shape[0]):
+        ans[i] = np.sqrt(a[i][0]*a[i][0] + a[i][1]*a[i][1])
+    return ans
+
+@njit
+def v_angle(a, v2):
+    ans = np.zeros(a.shape[0], dtype=np.float64)
+    n1 = v_norm(a)
+    v2 = v2.astype(np.float64)
+    n2 = np.linalg.norm(v2)
+    if n2 == 0:
+        return ans
+    for i in prange(a.shape[0]):
+        if n1[i] == 0:
+            ans[i] = 0
+            continue
+        ans[i] = np.degrees(np.math.acos(clip(np.dot(a[i], v2) / (n1[i] * n2), np.float64(-1), np.float64(1))))
+    return ans
+
+@njit
 def find_tongue_tip(img, init_vec=np.array([-1, 1])):
     # get c1 as COM of the whole mask
     mask = np.column_stack(np.where(img > 0)[::-1])
-    c1 = np.mean(mask, axis=0)
+    c1 = centroid(mask)
 
     # finding 75th percentile distance from c1
-    dists = np.linalg.norm(c1 - mask, axis=1)
+    dists = v_norm(c1 - mask)
     dist75 = int(sorted(dists)[int(0.75 * len(dists))])
 
     # candidate set 1 as pixels farther than dist75 and within 45 deg of v1, v1 as chosen constant
     v1 = init_vec 
-    cand1 = list(filter(lambda p : np.linalg.norm(v1 - (p-c1)) > dist75 and angle(p-c1, v1) < 45, mask))
+    cand1 = mask[np.where(np.logical_and(v_norm(mask - c1) > dist75, v_angle(mask - c1, v1) < 45))]
 
     if len(cand1) == 0:
         return c1
-    # candidate set 2 as pixels within 15 deg of v2 and on mask boundary, where v2 is c2-c1 with c2 as COM of cand1
-    c2 = np.mean(cand1, axis=0) 
+    c2 = centroid(cand1) 
     v2 = c2 - c1
-    cand2 = list(filter(lambda p : angle(p-c1, v2) < 15 and is_boundary(img, p), cand1))
+    cand2 = cand1[np.where(np.logical_and(v_angle(cand1-c1, v2) < 15, v_is_boundary(img, cand1)))]
 
     if len(cand2) == 0:
         return c2
     # tongue tip is COM of cand2
-    return np.mean(cand2, axis=0) 
+    return centroid(cand2) 
 
 def load_bool_img(path):
-    img = plt.imread(path).astype(bool)
-    return img
+    img = plt.imread(path)
+
+    # copilot says this lol
+    grayscale_img = np.dot(img[...,:3], [0.2989, 0.5870, 0.1140])
+    
+    threshold = 0.5
+    binarized_img = grayscale_img > threshold
+    return binarized_img.astype(bool)
 
 def plot_tip(img, tip):
     plt.imshow(img, cmap="gray")
@@ -52,10 +88,10 @@ def plot_tip(img, tip):
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser(description="Find tongue tip from binary mask.")
-    parser.add_argument("--in", required=True, help="Input image of binary mask")
+    parser.add_argument("--in_path", required=True, help="Input image of binary mask")
     parser.add_argument("--show", action="store_true", help="Show the image with the tongue tip")
     args = parser.parse_args()
-    img = load_bool_img(args)
+    img = load_bool_img(args.in_path)
     tip = find_tongue_tip(img)
     if args.show:
         plot_tip(img, tip)
